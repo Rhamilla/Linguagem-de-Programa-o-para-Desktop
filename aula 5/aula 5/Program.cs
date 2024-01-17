bcf954e2c27e4c3f917f778afb69dbb0b55460d3
Rhamillla R. Martins <105660825+Rhamilla@users.noreply.github.com>
2024-01-17 10:55:59 -0300

    Add files via upload

---
+++ aula 5/aula 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] pilotos = new string[4]{"Ayrton Senna", "Michael Schumacher","Lewis Hamilton", "Alain Prost"};
+            Console.WriteLine(pilotos[3]);
+            Console.WriteLine();
+            pilotos[3] = "Rubens Barrichello";
+            foreach(string piloto in pilotos)
+            {
+                Console.WriteLine(piloto);
+             
+            }
+            Console.ReadLine();
+        }
+    }
+}
